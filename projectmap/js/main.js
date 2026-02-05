dadf8640ba9ba317b4c63abb07f022ce0883496d
1vo-x33 <6047730@mborijnland.nl>
2026-02-05 10:08:26 +0100

    fix: deleted airplanes

--- projectmap/js/main.js
+++ projectmap/js/main.js
@@ -1,32 +1,3 @@
-let id = null;
-function fly() {
-  let elem = document.getElementById("spitfire");
-  elem.style.position = 'absolute';  
-  let angle = 0;
-  let centerX = window.innerWidth / 2 - 50;
-  let centerY = window.innerHeight / 2 - 50;
-  let radiusX = 200;
-  let radiusY = 100;
-  clearInterval(id);
-  id = setInterval(frame, 10);
-  function frame() {
-    if (angle >= 8 * Math.PI) {
-        clearInterval(id);
-        elem.style.position = '';
-        elem.style.top = '';
-        elem.style.left = '';
-        elem.style.zIndex = '1';
-    } else {
-      elem.style.position = 'absolute';
-      elem.style.zIndex='10';
-      angle += 0.05;
-      let x = centerX + radiusX * Math.sin(angle);
-      let y = centerY + radiusY * Math.sin(2 * angle);
-      elem.style.left = x + 'px';
-      elem.style.top = y + 'px';
-    }
-  }
-}
 let grass = [] 
 
 function setup(){
@@ -43,7 +14,6 @@ function setup(){
 }
 
 function draw(){
-    console.log('draw running');
     background(255);  // White background
     for (let g of grass){
         g.show()
