342212905ad755ccc304526f9a0d024633796d48
1vo-x33 <6047730@mborijnland.nl>
2026-02-05 09:44:15 +0100

    Prototype grass
    Commented out, work in progress

--- projectmap/js/main.js
+++ projectmap/js/main.js
@@ -27,3 +27,52 @@ function fly() {
     }
   }
 }
+// let grass = [] 
+
+// function setup(){
+//   createCanvas(windows.innerHeight, windows.innerWidth);
+// }
+
+// function draw(){
+//     background(30);
+//     for (let g of rains){
+//         g.show()
+//         g.update()
+//         g.checkMouse(); //Fix this
+//     }
+
+//     for (let i =0; i < 10; i++){
+//         grass.push(new Grass(random(width)))
+//     }
+
+//     print(grass.length)
+// }
+// class Grass{
+//     constructor(x, y){
+//         this.pos = createVector(x, y)
+//         this.vel = createVector(0, random(7, 10))
+//         this.len = random(15, 30)
+//         this.thick = random(255)
+//         this.color = color(34, 139, 34);
+//     }
+
+//     show(){
+//         stroke(this.color, this.thick)
+//         line(this.pos.x, this.pos.y, this.pos.x, this.pos.y-this.len)
+//     }
+//     update(){
+//         this.pos.add(this.vel)
+//         if (this.pos.y > height+100){
+//             grass.shift()
+//         }
+//     }
+//     checkMouse() {
+//         let d = dist(this.pos.x, this.pos.y, mouseX, mouseY);
+//         if (d < 50) { // Repel if within 50 pixels
+//             let repel = p5.Vector.sub(this.pos, createVector(mouseX, mouseY));
+//             repel.setMag(5); // Strength of the repelling force
+//             this.pos.add(repel);
+//         }
+//     }
+// }
+
