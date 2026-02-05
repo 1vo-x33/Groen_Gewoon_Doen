ac2a3d80683960eb9b755b34a2a3f3f051d9aee3
1vo-x33 <6047730@mborijnland.nl>
2026-02-05 10:00:10 +0100

    added falling grass (rain)

--- projectmap/js/main.js
+++ projectmap/js/main.js
@@ -27,52 +27,51 @@ function fly() {
     }
   }
 }
-// let grass = [] 
+let grass = [] 
 
-// function setup(){
-//   createCanvas(windows.innerHeight, windows.innerWidth);
-// }
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight);
+  let canvas = document.querySelector('canvas');
+  canvas.style.position = 'fixed';
+  canvas.style.top = '0';
+  canvas.style.left = '0';
+  canvas.style.zIndex = '-1';
+  // Add initial grass
+  for (let i = 0; i < 20; i++) {
+    grass.push(new Grass(random(width)));
+  }
+}
 
-// function draw(){
-//     background(30);
-//     for (let g of rains){
-//         g.show()
-//         g.update()
-//         g.checkMouse(); //Fix this
-//     }
+function draw(){
+    console.log('draw running');
+    background(255);  // White background
+    for (let g of grass){
+        g.show()
+        g.update()
+    }
 
-//     for (let i =0; i < 10; i++){
-//         grass.push(new Grass(random(width)))
-//     }
+    if (grass.length < 50) {
+        grass.push(new Grass(random(width)));
+    }
+}
+class Grass{
+    constructor(x){
+        this.pos = createVector(x, random(-30, 0))
+        this.vel = createVector(0, random(7, 10))
+        this.len = random(15, 30)
+        this.color = color(34, 139, 34);
+    }
 
-//     print(grass.length)
-// }
-// class Grass{
-//     constructor(x, y){
-//         this.pos = createVector(x, y)
-//         this.vel = createVector(0, random(7, 10))
-//         this.len = random(15, 30)
-//         this.thick = random(255)
-//         this.color = color(34, 139, 34);
-//     }
-
-//     show(){
-//         stroke(this.color, this.thick)
-//         line(this.pos.x, this.pos.y, this.pos.x, this.pos.y-this.len)
-//     }
-//     update(){
-//         this.pos.add(this.vel)
-//         if (this.pos.y > height+100){
-//             grass.shift()
-//         }
-//     }
-//     checkMouse() {
-//         let d = dist(this.pos.x, this.pos.y, mouseX, mouseY);
-//         if (d < 50) { // Repel if within 50 pixels
-//             let repel = p5.Vector.sub(this.pos, createVector(mouseX, mouseY));
-//             repel.setMag(5); // Strength of the repelling force
-//             this.pos.add(repel);
-//         }
-//     }
-// }
+    show(){
+        stroke(this.color)
+        strokeWeight(2);
+        line(this.pos.x, this.pos.y, this.pos.x, this.pos.y-this.len)
+    }
+    update(){
+        this.pos.add(this.vel)
+        if (this.pos.y > height+100){
+            grass.shift()
+        }
+    }
+}
 
