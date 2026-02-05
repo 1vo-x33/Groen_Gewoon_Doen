d5593e41a381db193973bc76a1da19fe6e23565c
1vo-x33 <6047730@mborijnland.nl>
2026-02-05 09:19:29 +0100

    Sptifire flying animation

--- projectmap/js/main.js
+++ projectmap/js/main.js
@@ -1,1 +1,26 @@
-
+let id = null;
+function fly() {
+  let elem = document.getElementById("spitfire");
+  elem.style.position = 'absolute';  
+  let angle = 0;
+  let centerX = window.innerWidth / 2 - 50;
+  let centerY = window.innerHeight / 2 - 50;
+  let radiusX = 200;
+  let radiusY = 100;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (angle >= 8 * Math.PI) {
+        clearInterval(id);
+        elem.style.position = '';
+        elem.style.top = '';
+        elem.style.left = '';
+    } else {
+      angle += 0.05;
+      let x = centerX + radiusX * Math.sin(angle);
+      let y = centerY + radiusY * Math.sin(2 * angle);
+      elem.style.left = x + 'px';
+      elem.style.top = y + 'px';
+    }
+  }
+}
